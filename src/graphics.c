60b7a4b2a273ec7dbe3f3e8654dec5bf99859dc1
Adrian Pistol <vifino@tty.sh>
2017-12-29 00:07:58 +0100

    More graphics things to fuck up.

--- src/graphics.c
+++ src/graphics.c
@@ -7,42 +7,77 @@
 #include <stdlib.h>
 
 // TODO: replace this. 20kdc, looking at you.
-void graphics_drawline(byte x1, byte y1, byte x2, byte y2, RGB *color)
+int graphics_drawline(byte x1, byte y1, byte x2, byte y2, RGB *color)
 {
-  int i,dx,dy,sdx,sdy,dxabs,dyabs,x,y,px,py;
+	int ret,i,dx,dy,sdx,sdy,dxabs,dyabs,x,y,px,py;
 
-  dx =x2 - x1;      /* the horizontal distance of the line */
-  dy =y2 - y1;      /* the vertical distance of the line */
-  dxabs = abs(dx);
-  dyabs = abs(dy);
-  sdx = sign(dx);
-  sdy = sign(dy);
-  x = dyabs >> 1;
-  y = dxabs >> 1;
-  px = x1;
-  py = y1;
+	dx =x2 - x1;ttt/* the horizontal distance of the line */
+	dy =y2 - y1;ttt/* the vertical distance of the line */
+	dxabs = abs(dx);
+	dyabs = abs(dy);
+	sdx = sign(dx);
+	sdy = sign(dy);
+	x = dyabs >> 1;
+	y = dxabs >> 1;
+	px = x1;
+	py = y1;
 
-  matrix_set(px, py, color);
+	ret = matrix_set(px, py, color);
+	if (ret != 0) return ret;
 
-  if (dxabs >= dyabs) { /* the line is more horizontal than vertical */
-    for(i = 0; i < dxabs; ++i) {
-      y += dyabs;
-      if (y >= dxabs) {
-        y -= dxabs;
-        py += sdy;
-      }
-      px += sdx;
-      matrix_set(px, py, color);
-    }
-  } else { // the line is more vertical than horizontal
-    for(i = 0;i < dyabs; ++i) {
-      x += dxabs;
-      if (x >= dyabs) {
-        x -= dyabs;
-        px += sdx;
-      }
-      py += sdy;
-      matrix_set(px, py, color);
-    }
-  }
+	if (dxabs >= dyabs) { /* the line is more horizontal than vertical */
+		for(i = 0; i < dxabs; ++i) {
+			y += dyabs;
+			if (y >= dxabs) {
+				y -= dxabs;
+				py += sdy;
+			}
+			px += sdx;
+			ret = matrix_set(px, py, color);
+			if (ret != 0) return ret;
+		}
+	} else { // the line is more vertical than horizontal
+		for(i = 0;i < dyabs; ++i) {
+			x += dxabs;
+		if (x >= dyabs) {
+				x -= dyabs;
+				px += sdx;
+			}
+			py += sdy;
+			ret = matrix_set(px, py, color);
+			if (ret != 0) return ret;
+		}
+	}
+	return 0;
+}
+
+// stolen from wikipedia because idk.
+void graphics_drawcircle(byte x0, byte y0, byte radius, RGB *color) {
+	byte x = radius-1;
+	byte y = 0;
+	int dx = 1;
+	int dy = 1;
+	int err = dx - (radius << 1);
+
+	while (x >= y) {
+		matrix_set(x0 + x, y0 + y, color);
+		matrix_set(x0 + y, y0 + x, color);
+		matrix_set(x0 - y, y0 + x, color);
+		matrix_set(x0 - x, y0 + y, color);
+		matrix_set(x0 - x, y0 - y, color);
+		matrix_set(x0 - y, y0 - x, color);
+		matrix_set(x0 + y, y0 - x, color);
+		matrix_set(x0 + x, y0 - y, color);
+
+		if (err <= 0) {
+			y++;
+			err += dy;
+			dy += 2;
+		}
+		if (err > 0) {
+			x--;
+			dx += 2;
+			err += dx - (radius << 1);
+		}
+	}
 }
--- src/graphics.h
+++ src/graphics.h
@@ -1,3 +1,4 @@
 #include <types.h>
 
 int graphics_drawline(byte x0, byte y0, byte x1, byte y1, RGB *color);
+int graphics_drawcircle(byte x0, byte y0, byte radius, RGB *color);
