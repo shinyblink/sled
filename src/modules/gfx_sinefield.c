7ad607fdff351cc147dbed5d0813bc674dd7b7ee
Dave Kliczbor <dave@fsinfo.cs.tu-dortmund.de>
2018-04-30 01:23:26 +0200

    Added pattern gfx_sinefield

---
+++ src/modules/gfx_sinefield.c
@@ -0,0 +1,83 @@
+/*
+ * sinefield port, much stolen from https://github.com/orithena/Arduino-LED-experiments/blob/master/MatrixDemo/MatrixDemo.ino
+ *
+ * Even the original author, @orithena, has no clue how this effect works. He just experimented and this came out.
+ *
+ */
+
+#include <types.h>
+#include <matrix.h>
+#include <timers.h>
+#include <stddef.h>
+#include <mathey.h>
+#include <math.h>
+
+#define FPS 60
+#define FRAMETIME (T_SECOND / FPS)
+#define FRAMES (RANDOM_TIME * FPS)
+
+/*** management variables ***/
+
+static int modno;
+static int frame;
+static ulong nexttick;
+
+/*** matrix info (initialized in init()) ***/
+
+static int mx, my;		// matrix size
+
+/*** module init ***/
+
+int init(int moduleno, char* argstr) {
+	mx = matrix_getx();
+	my = matrix_gety();
+	if (mx < 2)
+		return 1;
+	if (my < 2)
+		return 1;
+	modno = moduleno;
+	return 0;
+}
+
+
+/*** main drawing loop ***/
+
+int draw(int argc, char* argv[]) {
+	if (frame == 0)
+		nexttick = udate();
+
+	float step = (float)(((udate()) >> 16) & 0x00007FFF);
+	//printf("[%8.1f]", step);
+	byte hue = 0;
+	for( byte x = 0; x < mx; x++ ) {
+		hue = step + (37 * sinf( ((x*step)/(11*M_PI)) * 0.04 ) );
+		for( byte y = 0; y < my; y++ ) {
+			hue += 17 * sinf(y/(5*M_PI));
+			RGB color = HSV2RGB(HSV(
+				(byte)(hue + ((unsigned long)step & 0x000000FF)), 
+				255, //(byte)(192 - (63*cosf((hue+step)*M_PI*0.004145))), 
+				(byte)(255*sinf((hue+step)*M_PI*0.003891))
+			));
+			matrix_set(x,y,&color);
+		}
+	}
+
+	// render it out
+	matrix_render();
+
+	// manage framework variables
+	if (frame >= FRAMES) {
+		frame = 0;
+		return 1;
+	}
+	frame++;
+	nexttick += FRAMETIME;
+	timer_add(nexttick, modno, 0, NULL);
+	return 0;
+}
+
+/*** module deconstructor ***/
+
+int deinit() {
+	return 0;
+}
