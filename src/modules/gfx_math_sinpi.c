c0b548c8a1f590ea9aa756549faf93fd7d00f2e4
Markus Schmidl <markus.schmidl@ymail.com>
2018-05-03 13:52:57 +0200

    less nasty, but still voodoo

--- src/modules/gfx_math_sinpi.c
+++ src/modules/gfx_math_sinpi.c
@@ -20,7 +20,6 @@ static int modno;
 static int pos;
 static int frame;
 static ulong nexttick;
-static int _y;
 
 int init(int moduleno, char* argstr) {
 	if (matrix_getx() < 3)
@@ -39,33 +38,19 @@ void reset() {
 
 int draw(int argc, char* argv[]) {
 	vec2 p, _p;
-	int x;
-	int y, dy;
+	int dy;
 
 	matrix_clear();
 
-	for (p.x = 0; p.x <= XSCALE; p.x++) {
+	for (p.x = -1; p.x <= XSCALE; p.x++) {
 		p.y = - round((YSCALE - 1) / 2 * (sin((p.x + pos) * M_PI * 2.0f / (double) XSCALE) - 1));
-		/* what the hell why is p.x not equal to 0? */
-		if (frame == 0 && p.x <= 1)
-			_y = - round((YSCALE - 1) / 2 * (sin((p.x + pos - 1) * M_PI * 2.0f / (double) XSCALE) - 1));
 		matrix_set(p.x, p.y, &white);
-		if (p.x == 0) {
-			_p.x = -1;
-			_p.y = _y;
-			_y = p.y;
-		}
 		vec2 d = vadd(_p, vmul(p, -1));
 		dy = round(d.y);
 		d = vmul(d, 1.0/d.y);
-		for (int i = 1; abs(dy)>1, i<abs(dy); ++i) {
-			y = (int) (fmin(p.y,_p.y) + i*d.y);
-			x = (int) (fmax(p.x, _p.x) - i*d.x);
-			if (dy < 0 && y < _y && x == 0) 
-					continue;
-			matrix_set(x, y, &white);
-		}
-		memcpy(&_p, &p, sizeof(vec2));
+		for (int i = 1; abs(dy)>1, i<abs(dy); ++i)
+			matrix_set((int) ((p.x==0&&dy>0)?_p.x:p.x + i*d.x), (int) (fmin(p.y,_p.y) + i*d.y), &white);
+		memcpy(&_p, &p, sizeof(p));
 	}
 
 	matrix_render();
