7d34321dba53d18a9e8856d22fac950416aa4c7c
Jakob Löw <github@m4gnus.de>
2018-05-12 15:05:16 +0200

    Add IP address display module (#18)
    * add a module for displaying the own ip address (for now only ipv4 of eth0)
    
    * display all ips of all interfaces in multiple lines
    
    * add gfx_ip to the GNUmakefile
    
    * allow blacklisting the display of specific network interfaces via an environment variable
    
    * clear the screen befre drawing to it

--- GNUmakefile
+++ GNUmakefile
@@ -5,6 +5,7 @@ MODULES_AVAILABLE := gfx_random_static gfx_random_rects gfx_twinkle gfx_gol
 MODULES_AVAILABLE += gfx_rainbow gfx_math_sinpi gfx_text gfx_plasma gfx_checkerboard
 MODULES_AVAILABLE += gfx_balls gfx_clock gfx_sinematrix gfx_error gfx_partirush
 MODULES_AVAILABLE += gfx_matrix gfx_cube gfx_mandelbrot gfx_golc gfx_sinefield gfx_affinematrix
+MODULES_AVAILABLE += gfx_ip
 
 MODULES_AVAILABLE += out_dummy out_sdl2 out_rpi_ws2812b out_udp out_fb out_rpi_hub75
 MODULES_AVAILABLE += out_sf75_bi_spidev
@@ -16,6 +17,7 @@ MODULES := gfx_random_static gfx_random_rects gfx_twinkle gfx_gol
 MODULES += gfx_rainbow gfx_math_sinpi gfx_plasma gfx_checkerboard
 MODULES += gfx_balls gfx_clock gfx_sinematrix gfx_error gfx_partirush
 MODULES += gfx_matrix gfx_cube gfx_mandelbrot gfx_golc gfx_sinefield gfx_affinematrix
+MODULES += gfx_ip
 
 MODULES += bgm_fish bgm_opc bgm_pixelflut
 MODULES += flt_gamma_correct flt_flip_x flt_flip_y flt_scale
---
+++ src/modules/gfx_ip.c
@@ -0,0 +1,109 @@
+#include <types.h>
+#include <matrix.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include "text.h"
+
+#define FRAMETIME (T_SECOND)
+#define FRAMES (10)
+#define CHARS_FULL 8 // 20:15:09, must also hold 20:15 (small)
+
+static text **lines = NULL;
+static int linecount = 0;
+static int columncount = 0;
+static const char *ignored_interfaces;
+
+void reset_lines()
+{
+	for(int i = 0; i < linecount; i++) {
+		text_free(lines[i]);
+		lines[i] = NULL;
+	}
+}
+
+int init(int modno, char *argstr) {
+	if (matrix_gety() < 7)
+		return 1; // not enough Y to be usable
+
+	linecount = matrix_gety() / 8;
+	columncount = matrix_getx() / 3;
+
+	lines = calloc(linecount, sizeof(text *));
+
+	ignored_interfaces = getenv("SLED_IP_BLACKLIST");
+	if(ignored_interfaces == NULL)
+		ignored_interfaces = "lo";
+
+	return 0;
+}
+
+void reset() {
+    char buff[INET6_ADDRSTRLEN];
+	char displaybuff[columncount];
+	buff[0] = 0;
+
+	reset_lines();
+
+	struct ifaddrs *ifap;
+	getifaddrs(&ifap);
+
+	int i = 0;
+	for(struct ifaddrs *curr = ifap; curr != NULL; curr = curr->ifa_next) {
+		const char *ignored = strstr(ignored_interfaces, curr->ifa_name);
+		if(ignored != NULL) {
+			size_t len = strlen(curr->ifa_name);
+			if(ignored[len] == 0 || ignored[len] == ',')
+				continue;
+		}
+
+		struct sockaddr_in *addr = (struct sockaddr_in *)curr->ifa_addr;
+		if(inet_ntop(curr->ifa_addr->sa_family, &(addr->sin_addr), buff, INET6_ADDRSTRLEN) != NULL) {
+			snprintf(displaybuff, columncount, "%s", curr->ifa_name);
+			lines[i] = text_render(displaybuff);
+			i++;
+
+			snprintf(displaybuff, columncount, "    %s", buff);
+			lines[i] = text_render(displaybuff);
+			i++;
+		}
+	}
+
+	freeifaddrs(ifap);
+}
+
+int draw(int argc, char **argv) {
+	RGB black = RGB(0, 0, 0);
+	for(int y = 0; y < matrix_gety(); y++) {
+		for(int x = 0; x < matrix_getx(); x++) {
+			matrix_set(x, y, &black);
+		}
+	}
+
+	for(int i = 0; i < linecount; i++) {
+		if(lines[i] == NULL)
+			continue;
+
+		for (int y = 0; y < matrix_gety() && y < 8; y++) {
+			for (int x = 0; x < matrix_getx(); x++) {
+				int v = text_point(lines[i], x, y) ? 255 : 0;
+				RGB colour = RGB(v, v, v);
+				matrix_set(x, i * 8 + y, &colour);
+			}
+		}
+	}
+
+	matrix_render();
+	return 0;
+}
+
+int deinit() {
+	reset_lines();
+	return 0;
+}
