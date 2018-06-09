217f95cd15da0b9086a2d98a5aa1152d09037d01
20kdc <asdd2808@gmail.com>
2018-06-09 02:24:43 +0100

    BROKEN PIXELFLUT, DO NOT PUSH

--- src/modules/bgm_pixelflut.c
+++ src/modules/bgm_pixelflut.c
@@ -1,8 +1,8 @@
-// OPC: Open Pixel Control output
-// This allows using SLED as an OPC output.
-// Shoudl an OPC connection connect,
+// PIXELFLUT: Pixelflut output
+// This allows using SLED as a Pixelflut output.
+// Should a Pixelflut connection connect,
 //  the module will start.
-// Should all OPC connections disconnect,
+// Should all Pixelflut connections disconnect,
 //  the module will end.
 
 #ifdef __linux__
@@ -27,28 +27,43 @@
 #include "modloader.h"
 #include "asl.h"
 
-// It is *IMPOSSIBLE* for a px client to send a length that escapes this buffer.
-static byte px_array[65536];
+// 😥
+static RGB * px_array;
 
 static int px_shutdown_fd_mt, px_shutdown_fd_ot;
 // px_mtcountdown is the time until we decide to end. It's main-thread-only.
 static int px_moduleno, px_mtcountdown;
 static int px_pixelcount, px_clientcount;
 
+// To prevent side effects on other modules,
+//  this variable must be locked at the right times.
+// It is only ever written from the main thread,
+//  and is only ever read from the network thread.
+// Keep in mind that the network thread takes direct control over the matrix.
+static int px_bgminactive;
+static oscore_mutex px_bgmi_mutex;
+
 static int px_mx, px_my;
 static ulong px_mtlastframe;
 static oscore_task px_task;
 
+
 #define PX_MTCOUNTDOWN_MAX 100
+// 10ms (100FPS)
 #define FRAMETIME 10000
 #define PX_PORT 1337
-//#define PX_SNAKE
+// The maximum, including 0, size of a line.
+#define PX_LINESIZE 64
 
 typedef struct {
 	int socket; // The socket
 	int off_x;
 	int off_y;
 	void * next; // The next client
+	// The line buffer
+	char line[PX_LINESIZE];
+	// Overflow biscuit, just in case.
+	char ovl_biscuit;
 } px_client_t;
 
 // shamelessly ripped from pixelnuke
@@ -114,147 +129,179 @@ static void poke_main_thread(void) {
 	}
 }
 
-// returns 0 on successful line read
-static int px_client_readln(px_client_t * client, char * line, size_t maxread) {
-	char * line_read = line;
-	do {
-		ssize_t r = read(client->socket, line_read, 1);
-		if (r <= 0) return -1;
+// Executes the line given.
+// Please ignore the return value.
+// I have a sneaking suspicion the returns were in the original code.
+// Anyway, as this uses matrix_set, and thus checks if the BGM is active/inactive, make sure to use this inside the BGM activity lock.
+static int px_client_executeline(px_client_t * client) {
+	const char * line = client->line;
+	// In the original version, this was ripped from pixelnuke,
+	//  and it remains that way, but hopefully I've changed it enough.
+	if (line[0] == 'P' && line[1] == 'X') {
+		const char * ptr = line + 3;
+		const char * endptr = ptr;
 
-		line_read++;
-		maxread--;
-	} while (maxread != 0 && *(line_read-1) != '\n');
-	return 0;
-}
+		uint32_t x = fast_strtoul10(ptr, &endptr);
+		if (endptr == ptr) {
+			net_err(client, "ERROR: Invalid command (expected decimal as first parameter)");
+			return 1;
+		}
+		if (!*endptr) {
+			net_err(client, "ERROR: Invalid command (second parameter required)");
+			return 1;
+		}
 
-// Returns true to remove the client.
-static int px_client_update(px_client_t * client) {
-	char line[20];
-	if (px_client_readln(client, line, sizeof(line))) {
-		if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
-			// Okay!
+		endptr++; // eat space (or whatever non-decimal is found here)
+
+		uint32_t y = fast_strtoul10((ptr = endptr), &endptr);
+		if (endptr == ptr) {
+			net_err(client, "Invalid command (expected decimal as second parameter)");
+			return 1;
+		}
+
+		// Please note that these are signed variables being added to unsigned variables.
+		// Almost all invalid coordinates will be extremely high.
+		x += client->off_x;
+		y += client->off_y;
+
+		int inbounds = (x < px_mx) && (y < px_my);
+
+		size_t index = x + (y * px_mx);
+
+		// PX <x> <y> -> Get RGB color at position (x,y) or '0x000000' for out-of-range queries
+		if (!*endptr) {
+			char str[64];
+
+			RGB pixel = RGB(0, 0, 0);
+			if (inbounds)
+				pixel = px_array[index];
+			sprintf(str, "PX %u %u %02X%02X%02X", x, y, pixel.red, pixel.green, pixel.blue);
+			net_send(client, str);
 			return 0;
 		}
-		return 1;
-	} else { // parse line
-		// ripped from pixelnuke
-		if (line[0] == 'P' && line[1] == 'X') {
-			const char * ptr = line + 3;
-			const char * endptr = ptr;
-			errno = 0;
 
-			uint32_t x = fast_strtoul10(ptr, &endptr);
-			if (endptr == ptr) {
-				net_err(client, "ERROR: Invalid command (expected decimal as first parameter)");
-				return 1;
-			}
-			if (*endptr == '\n') {
-				net_err(client, "ERROR: Invalid command (second parameter required)");
-				return 1;
-			}
+		endptr++; // eat space (or whatever non-decimal is found here)
 
-			endptr++; // eat space (or whatever non-decimal is found here)
+		// PX <x> <y> BB|RRGGBB|RRGGBBAA
+		uint32_t c = fast_strtoul16((ptr = endptr), &endptr);
+		if (endptr == ptr) {
+			net_err(client, "Third parameter missing or invalid (should be hex color)");
+			return 1;
+		}
 
-			uint32_t y = fast_strtoul10((ptr = endptr), &endptr);
-			if (endptr == ptr) {
-				net_err(client, "Invalid command (expected decimal as second parameter)");
-				return 1;
-			}
+		RGB pixel;
+		if (endptr - ptr == 6) {
+			// 0x00RRGGBB
+			pixel.red = c >> 16;
+			pixel.green = c >> 8;
+			pixel.blue = c;
+		} else if (endptr - ptr == 8) {
+			// 0xRRGGBBAA
+			pixel.red = c >> 24;
+			pixel.green = c >> 16;
+			pixel.blue = c >> 8;
+		} else if (endptr - ptr == 2) {
+			// 0x000000Gr
+			pixel.red = c;
+			pixel.green = c;
+			pixel.blue = c;
+		} else {
+			net_err(client, "Color hex code must be 2, 6 or 8 characters long (WW, RGB or RGBA)");
+			return 1;
+		}
 
-			// PX <x> <y> -> Get RGB color at position (x,y) or '0x000000' for out-of-range queries
-			if (*endptr == '\n') {
-				char str[64];
+		px_pixelcount++;
 
-				uint32_t pixel = *(uint32_t *)&px_array[((x + client->off_x)*px_mx + (y + client->off_y)) * 3];
-				sprintf(str, "PX %u %u %06X", x, y, pixel & 0xFFFFFF);
-				net_send(client, str);
-				poke_main_thread();
-				return 0;
-			}
+		if (!inbounds)
+			return 0;
 
-			endptr++; // eat space (or whatever non-decimal is found here)
+		if (!px_bgminactive)
+			matrix_set(x, y, &pixel);
+		px_array[index] = pixel;
+	} else if (fast_str_startswith("OFFSET", line)) {
+		const char * ptr = line + 7;
+		const char * endptr = ptr;
 
-			// PX <x> <y> BB|RRGGBB|RRGGBBAA
-			uint32_t c = fast_strtoul16((ptr = endptr), &endptr);
-			if (endptr == ptr) {
-				net_err(client,
-								"Third parameter missing or invalid (should be hex color)");
-				return 1;
-			}
+		uint32_t x = fast_strtoul10(ptr, &endptr);
+		if (endptr == ptr) {
+			net_err(client, "ERROR: Invalid command (expected decimal as first parameter)");
+			return 1;
+		}
+		if (!*endptr) {
+			net_err(client, "ERROR: Invalid command (second parameter required)");
+			return 1;
+		}
 
-			if (endptr - ptr == 6) {
-				// done
-			} else if (endptr - ptr == 8) {
-				// RGBA -> RGB (we dont do alpha)
-				c = c >> 8;
-			} else if (endptr - ptr == 2) {
-				// WW -> RGBA // UNTESTED, THIS WILL BREAK
-				c = (c << 24) + (c << 16) + (c << 8) + 0xff;
-			} else {
-				net_err(client,
-								"Color hex code must be 2, 6 or 8 characters long (WW, RGB or RGBA)");
-				return 1;
-			}
+		endptr++; // eat space (or whatever non-decimal is found here)
 
-			px_pixelcount++;
-			px_array[((x + client->off_x)*px_mx + (y + client->off_y)) * 3]    = c >> 16;
-			px_array[((x + client->off_x)*px_mx + (y + client->off_y)) * 3 +1] = c >>  8;
-			px_array[((x + client->off_x)*px_mx + (y + client->off_y)) * 3 +2] = c;
-		} else if (fast_str_startswith("OFFSET", line)) {
-			const char * ptr = line + 7;
-			const char * endptr = ptr;
-			errno = 0;
+		uint32_t y = fast_strtoul10((ptr = endptr), &endptr);
+		if (endptr == ptr) {
+			net_err(client, "Invalid command (expected decimal as second parameter)");
+			return 1;
+		}
 
-			uint32_t x = fast_strtoul10(ptr, &endptr);
-			if (endptr == ptr) {
-				net_err(client, "ERROR: Invalid command (expected decimal as first parameter)");
-				return 1;
-			}
-			if (*endptr == '\n') {
-				net_err(client, "ERROR: Invalid command (second parameter required)");
-				return 1;
-			}
-
-			endptr++; // eat space (or whatever non-decimal is found here)
-
-			uint32_t y = fast_strtoul10((ptr = endptr), &endptr);
-			if (endptr == ptr) {
-				net_err(client, "Invalid command (expected decimal as second parameter)");
-				return 1;
-			}
-
-			// OFFSET <x> <y> -> set offset to position (x,y) for future use
-			if (*endptr == '\n') {
-				client->off_x = x;
-				client->off_y = y;
-				poke_main_thread();
-				return 0;
-			}
-		} else if (fast_str_startswith("SIZE", line)) {
-			char str[64];
-			snprintf(str, 64, "SIZE %d %d", px_mx, px_my);
-			net_send(client, str);
-		} else if (fast_str_startswith("STATS", line)) {
-			char str[128];
-			snprintf(str, 128, "STATS px:%u conn:%u", px_pixelcount,
-							 px_clientcount);
-			net_send(client, str);
-		} else if (fast_str_startswith("HELP", line)) {
-			net_send(client,
+		// OFFSET <x> <y> -> set offset to position (x,y) for future use
+		if (!*endptr) {
+			client->off_x = x;
+			client->off_y = y;
+			return 0;
+		}
+	} else if (fast_str_startswith("SIZE", line)) {
+		char str[64];
+		snprintf(str, 64, "SIZE %d %d", px_mx, px_my);
+		net_send(client, str);
+	} else if (fast_str_startswith("STATS", line)) {
+		char str[128];
+		snprintf(str, 128, "STATS px:%u conn:%u", px_pixelcount, px_clientcount);
+		net_send(client, str);
+	} else if (fast_str_startswith("HELP", line)) {
+		net_send(client,
 							 "\
 PX x y: Get color at position (x,y)\n\
 PX x y rrggbb(aa): Draw a pixel (alpha ignored)\n\
 SIZE: Get canvas size\n\
 STATS: Return statistics");
 
-		} else {
-			net_err(client, "Unknown command");
-			return 1;
-		}
-		// END ripped from pixelnuke
+	} else {
+		net_err(client, "Unknown command");
+		return 1;
+	}
+	// END ripped from pixelnuke
+	return 0;
+}
+
+// Returns true to remove the client.
+static int px_client_update(px_client_t * client) {
+	if (client->ovl_biscuit != 0xC0) {
+		fputs("Pixelflut overflow biscuit must always be 0xC0. Something went wrong. -- Pixelflut", stderr);
+		exit(1);
+	}
+	// Simplifies and speeds up things
+	char * line = client->line;
+	size_t linelen = strlen(line);
+	ssize_t addlen = read(client->socket, line + linelen, PX_LINESIZE - (1 + linelen));
+	int execcommand = 0;
+	if (addlen < 0) {
+		// All errors except these are assumed to mean the connection died.
+		if ((errno == EAGAIN) || (errno == EWOULDBLOCK))
+			return 0;
+		return 1;
+	} else {
+		// Zero-terminate the resulting string.
+		line[linelen + addlen] = 0;
+	}
+	// Any string appending that is occuring is done.
+	// Check for any newlines.
+	char * endptr;
+	while ((endptr = strchr(line, '\n'))) {
+		*endptr = 0;
+		// Line buffer prepped, execute.
+		px_client_executeline(client);
+		// Shift line left. We're not allowed to use strcpy due to potential overlap, so don't try it.
+		// Note that a full buffer always ends with two nulls - one for the newline and one for the actual end of buffer.
+		memmove(line, endptr + 1, strlen(endptr + 1) + 1);
 	}
 	poke_main_thread();
-
 	return 0;
 }
 
@@ -265,6 +312,8 @@ static int px_client_new(px_client_t ** list, int sock) {
 		close(sock);
 		return 0;
 	}
+	c->ovl_biscuit = 0xC0;
+	c->line[0] = 0;
 	c->socket = sock;
 	c->off_x = 0;
 	c->off_y = 0;
@@ -316,12 +365,12 @@ static void px_thread_func(void * n) {
 		int accepted = accept(server, NULL, NULL);
 		if (accepted >= 0) {
 			px_nbs(accepted);
-			if (px_client_new(&list, accepted))
-				FD_SET(accepted, &rset);
+			px_client_new(&list, accepted);
 		}
 		// select zeroes FDs >:(
 		FD_ZERO(&rset);
-		// Go through all clients!
+		oscore_mutex_lock(px_bgmi_mutex);
+		// Go through all clients, holding the BGMI lock so that the status of "are we in control of the matrix" cannot change.
 		px_client_t ** backptr = &list;
 		while (*backptr) {
 			if (px_client_update(*backptr)) {
@@ -334,6 +383,7 @@ static void px_thread_func(void * n) {
 				backptr = (px_client_t**) &((*backptr)->next);
 			}
 		}
+		oscore_mutex_unlock(px_bgmi_mutex);
 		FD_SET(px_shutdown_fd_ot, &rset);
 		FD_SET(server, &rset);
 		select(FD_SETSIZE, &rset, NULL, NULL, NULL);
@@ -358,52 +408,61 @@ int init(int moduleno, char* argstr) {
 
 	px_mx = matrix_getx();
 	px_my = matrix_gety();
+	px_array = calloc(px_mx * px_my, sizeof(RGB));
+	if (!px_array) {
+		// Insufficient RAM.
+		close(tmp[0]);
+		close(tmp[1]);
+		return 1;
+	}
 	// For whatever reason, the *receiver* is FD 0.
 	px_shutdown_fd_mt = tmp[1];
 	px_shutdown_fd_ot = tmp[0];
 	px_moduleno = moduleno;
+	px_bgminactive = 1;
+	px_bgmi_mutex = oscore_mutex_new();
 	px_task = oscore_task_create("bgm_pixelflut", px_thread_func, NULL);
 
 	return 0;
 }
 
 int draw(int argc, char ** argv) {
+	// To prevent weighing everything down, the mutex is only used here when px_bgminactive is being written.
+	// All writes occur from here, so consistency is assured.
 	if (argc) {
 		px_mtcountdown = PX_MTCOUNTDOWN_MAX;
 		px_mtlastframe = udate();
-	}
-	matrix_clear();
-	int indx = 0;
-	// Note the use of 65535 as the maximum amount (making 65534 the maximum index),
-	//  this aligns to the magic number 3.
-	int w = px_mx, h = matrix_gety();
-	for (int i = 0; i < w; i++) {
-		for (int j = 0; j < h; j++) {
-			byte r = px_array[indx++];
-			byte g = px_array[indx++];
-			byte b = px_array[indx++];
-			RGB rgb = RGB(r, g, b);
-#ifdef PX_SNAKE
-			matrix_set(i, (i & 1) ? j : h - (j + 1), &rgb);
-#else
-			matrix_set(i, j, &rgb);
-#endif
-			if (indx == 65535)
-				break;
+		if (px_bgminactive) {
+			oscore_mutex_lock(px_bgmi_mutex);
+			int indx = 0;
+			for (int j = 0; j < px_my; j++) {
+				for (int i = 0; i < px_mx; i++) {
+					matrix_set(i, j, px_array + indx);
+					indx++;
+				}
+			}
+			px_bgminactive = 0;
+			oscore_mutex_unlock(px_bgmi_mutex);
 		}
-		if (indx == 65535)
-			break;
 	}
+	// We still do matrix_render on the main thread.
+	// Mainly because not doing so would be a very quick road to blocking up the network thread.
 	matrix_render();
 	if ((--px_mtcountdown) > 0) {
 		timer_add(px_mtlastframe += FRAMETIME, px_moduleno, 0, NULL);
 		return 0;
 	}
+	oscore_mutex_lock(px_bgmi_mutex);
+	px_bgminactive = 1;
+	oscore_mutex_unlock(px_bgmi_mutex);
 	return 1;
 }
 
 void reset(void) {
-	// Nothing?
+	oscore_mutex_lock(px_bgmi_mutex);
+	// If it's 0, then quite a mess will result...
+	px_bgminactive = 1;
+	oscore_mutex_unlock(px_bgmi_mutex);
 }
 
 int deinit() {
@@ -412,5 +471,7 @@ int deinit() {
 		oscore_task_join(px_task);
 	close(px_shutdown_fd_mt);
 	close(px_shutdown_fd_ot);
+	oscore_mutex_free(px_bgmi_mutex);
+	free(px_array);
 	return 0;
 }
