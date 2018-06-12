0f255de48f9f20d484217d8082d01d00bacb2337
Adrian Pistol <vifino@tty.sh>
2018-06-12 16:22:39 +0200

    out_dummy: Fix CIMODE.

--- src/modules/out_dummy.c
+++ src/modules/out_dummy.c
@@ -49,14 +49,14 @@ int render(void) {
 ulong wait_until(ulong desired_usec) {
 	// Hey, we can just delegate work to someone else. Yay!
 #ifdef CIMODE
-	return 0;
+	return desired_usec;
 #else
 	return wait_until_core(desired_usec);
 #endif
 }
 
 void wait_until_break(void) {
-#ifndef CIMODE
+#ifdef CIMODE
 	wait_until_break_core();
 #endif
 }
