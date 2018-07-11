648214c8cd7965fb6e72e4250dcbad2a35b7ed9f
cyriax <brast@cs.uni-frankfurt.de>
2018-07-11 19:42:49 +0200

    gfx_mandelbrot: more variation in place and color

--- src/modules/gfx_mandelbrot.c
+++ src/modules/gfx_mandelbrot.c
@@ -31,6 +31,8 @@ static volatile int min;
 static volatile int max;
 static oscore_mutex lock;
 
+static byte color_offset = 0;
+
 static float initial_size = 0.1;
 static float initial_x = 0.314;
 static float initial_y = 0.587;
@@ -78,6 +80,11 @@ static int rescale(int x, int maxn, int newn) {
 inline float FADE(float start, float end,int part, int total){
 	return  start + SCALE(part,total)*(end-start);
 }
+inline float LOG_FADE(float start, float end,int part,int total){
+    float scale = 1.0;
+    for (int i=0;i<part;i++) scale /= 2.0;
+	return  end - scale*(end-start);
+}
 
 void reset(void) {
 	nexttick = udate();
@@ -85,22 +92,81 @@ void reset(void) {
 	frame = 0;
 	min = 0;
 	max = 0;
+    color_offset = randn(255);
+    switch (randn(6)){
+        case 0:
+            initial_size = 0.1;
+            initial_x = 0.314;
+            initial_y = 0.587;
+            end_size = 0.00362;
+            end_x = 0.35250;
+            end_y = 0.58206;
+            break;
+        case 1:
+            initial_size = 0.4;
+            initial_x = -1.18;
+            initial_y = 0;
+            end_size = 0.07;
+            end_x = -1.38;
+            end_y = 0;
+            break;
+        case 2:
+            initial_size = 0.058;
+            initial_x = -1.3581;
+            initial_y = -0.0654489;
+            end_size = 0.0041473;
+            end_x = -1.37270096;
+            end_y = -0.08882466;
+            break;
+        case 3:
+            initial_size = 0.01;
+            initial_x = -1.256255;
+            initial_y = -0.381321;
+            end_size = 0.6;
+            end_x = -1.2;
+            end_y = -0.3;
+            break;
+        case 4:
+            initial_size = 0.239;
+            initial_x = 0.361;
+            initial_y = -.056;
+            end_size = 0.02;
+            end_x = 0.395994;
+            end_y = -0.1337;
+            break;
+        case 5:
+            initial_size = 0.11;
+            initial_x = -0.512;
+            initial_y = 0.560;
+            end_size = 0.003;
+            end_x = -0.5490526;
+            end_y = 0.651128;
+            break;
+        case 6:
+            initial_size = 0.005;
+            initial_x = -0.563659;
+            initial_y = 0.6548416;
+            end_size = 0.005;
+            end_x = -0.5623978;
+            end_y = 0.6426125;
+            break;
+
+    }
 }
 
 void drawrow(void* row) {
 	int py = *(int*) row;
 	int px;
 
-	if (py < 0 || py >= mx) return;
-	//
+	if (py < 0 || py >= my) return;
 	float size = FADE(initial_size,end_size,frame,FRAMES);
 	float center_x = FADE(initial_x,end_x,frame,FRAMES);
 	float center_y = FADE(initial_y,end_y,frame,FRAMES);
 	float aspect_correction = SCALE(my,mx);
+    float y0 = FADE(center_y-size/2.0*aspect_correction,center_y+size/2.0*aspect_correction,py,my);
 
 	for (px = 0; px < mx; px++) {
 		float x0 = FADE(center_x-size/2.0,center_x+size/2.0,px,mx);
-		float y0 = FADE(center_y-size/2.0*aspect_correction,center_y+size/2.0*aspect_correction,py,my);
 
 		float x = 0;
 		float y = 0;
@@ -125,7 +191,7 @@ void drawrow(void* row) {
 			int mmin = min;
 			int mmax = max;
 			//oscore_mutex_unlock(lock);
-			byte scaled = rescale(i - mmin, mmax, 255);
+			byte scaled = (rescale(i - mmin, mmax, 255)+color_offset)%256;;
 			col = HSV2RGB(HSV(scaled, 255, 255));
 		}
 		matrix_set(px, py, col);
