7f35526c8b2404acc50f5b47bf745a642bb5f6c8
cyriax <brast@cs.uni-frankfurt.de>
2018-07-16 14:27:47 +0200

    gfx_sparkburn: astyle and static

--- src/modules/gfx_sparkburn.c
+++ src/modules/gfx_sparkburn.c
@@ -27,10 +27,10 @@ static char interesting = 0;
 void reset(void) {
     rmax = -1000;
     rmin = 1000;
-    for (int * p=field;p<field+mx*my;p++){
+    for (int * p=field; p<field+mx*my; p++) {
         int rr = rand();
         int r = 0;
-        for (int i=0;i<8;i++){
+        for (int i=0; i<8; i++) {
             r += (rr>>i*4)&0xf;
         }
         //r /= 2;
@@ -38,27 +38,27 @@ void reset(void) {
         rmin = (rmin>r)?r:rmin;
         *p = 1+r;
     }
-    for (int * p=field;p<field+mx*my;p++){
+    for (int * p=field; p<field+mx*my; p++) {
         *p -= (rmin);
     }
     threshold = (rmax-rmin);
-	nexttick = udate();
-	matrix_clear();
+    nexttick = udate();
+    matrix_clear();
     interesting = 1;
 }
 
-void calc(){
+static void calc() {
     interesting = 0;
     if (threshold) threshold--;
-    
-    for (int i=0;i<mx;i++){
-        for (int j=0;j<mx;j++){
+
+    for (int i=0; i<mx; i++) {
+        for (int j=0; j<mx; j++) {
             int * px = (field+i+j*mx);
-            if (*px == 255){
+            if (*px == 255) {
                 *px = 0;
                 interesting = 1;
             }
-            else if (*px >= threshold){
+            else if (*px >= threshold) {
 
                 *px *= 1.1;
                 if (*px >= 256) {
@@ -66,7 +66,7 @@ void calc(){
                 }
                 interesting = 1;
             }
-            else if (*px > 0){
+            else if (*px > 0) {
                 *px += 1;
                 interesting = 1;
             }
@@ -77,32 +77,32 @@ void calc(){
 
 int draw(int argc, char* argv[]) {
     calc();
-    for (int i=0;i<mx;i++){
-        for (int j=0;j<mx;j++){
+    for (int i=0; i<mx; i++) {
+        for (int j=0; j<mx; j++) {
             char px = *(field+i+j*mx);
             matrix_set(i,j,RGB(px,px,px));
         }
     }
 
-	matrix_render();
-    if (interesting == 0){
+    matrix_render();
+    if (interesting == 0) {
         return 1;
     }
 
-	nexttick += FRAMETIME;
-	timer_add(nexttick, modno, 0, NULL);
-	return 0;
+    nexttick += FRAMETIME;
+    timer_add(nexttick, modno, 0, NULL);
+    return 0;
 }
 
 int init(int moduleno, char* argstr) {
     mx = matrix_getx();
     my = matrix_gety();
     field = malloc(mx*my*sizeof(int));
-	modno = moduleno;
-	return 0;
+    modno = moduleno;
+    return 0;
 }
 
 int deinit() {
     free(field);
-	return 0;
+    return 0;
 }
