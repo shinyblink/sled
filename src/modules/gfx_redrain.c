7085be84138adb9c1b534ad16249ae8034ba10d7
cyriax <brast@cs.uni-frankfurt.de>
2018-08-23 00:07:27 +0200

    gfx_redrain: fat drops and nonuniform dropping

--- src/modules/gfx_redrain.c
+++ src/modules/gfx_redrain.c
@@ -11,7 +11,7 @@
 #include <graphics.h>
 #include <math.h>
 
-#define FPS 60
+#define FPS 100
 #define FRAMETIME (T_SECOND / FPS)
 #define FRAMES (RANDOM_TIME * FPS) * 10
 
@@ -35,7 +35,13 @@ typedef struct drop {
 
 static drop * drops;
 
-
+static void pdraw(int x, int y, int r, int g, int b){
+        if (x<0) x+=mx;
+        if (x>=mx) x-=mx;
+        if (y<0) y+=my;
+        if (y>=my) y-=my;
+        matrix_set(x,y,RGB(r,g,b));
+}
 
 int init(int moduleno, char* argstr) {
     mx = matrix_getx();
@@ -49,7 +55,7 @@ int init(int moduleno, char* argstr) {
 
 void reset(void) {
     nexttick = udate();
-    matrix_clear();
+    //matrix_clear();
     for (int i = 0;i<NO_OF_PARTICLES;i++){
         drops[i].x = randn(mx-1);
         drops[i].y = randn(my-1);
@@ -60,6 +66,7 @@ void reset(void) {
 
 static void randwalkdraw(){
     for (int i=0;i<NO_OF_PARTICLES;i++){
+        //if (randn(2)) continue;
         drops[i].y+=1;
         drops[i].x+=randn(2)-1;
         if (drops[i].x<0) drops[i].x+=mx;
@@ -69,7 +76,16 @@ static void randwalkdraw(){
         drops[i].redness+=randn(10)-5;
         if (drops[i].redness < 0) drops[i].redness =0;
         if (drops[i].redness > REDNESS_RANGE) drops[i].redness =REDNESS_RANGE;
-        matrix_set(drops[i].x,drops[i].y,RGB(REDNESS_OFFSET+drops[i].redness,0,0));
+        int red = REDNESS_OFFSET + drops[i].redness;
+        pdraw(drops[i].x,drops[i].y,red,0,0);
+#define FAT_DROPS
+#ifdef FAT_DROPS
+        pdraw(drops[i].x,drops[i].y+1,red*1.2,0,0);
+        pdraw(drops[i].x-1,drops[i].y,red*0.8,0,0);
+        pdraw(drops[i].x+1,drops[i].y,red*0.8,0,0);
+        pdraw(drops[i].x-1,drops[i].y+1,red*0.5,0,0);
+        pdraw(drops[i].x+1,drops[i].y+1,red*0.5,0,0);
+#endif
     }
 }
 
