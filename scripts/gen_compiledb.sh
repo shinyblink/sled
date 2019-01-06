12030483bb022588f77575a7a97a97b54df6d9cf
Fridtjof Mund <2780577+fridtjof@users.noreply.github.com>
2019-01-07 00:03:07 +0100

    Basic CLion support (#65)
    * ignore CLion files, bonus: pretty gitignore!
    
    * compiledb "script"
    
    * Makefile: ignore cat's return code to make compiledb output anything at all
    
    * gitignore: trim whitespace
    
    * gen_compiledb.sh: correct directory

---
+++ scripts/gen_compiledb.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This generates/updates a compile_commands.json for use with CLion (just open this directory with CLion)
+# you can get compiledb with pip:
+# pip install compiledb
+compiledb -n make
