5e2ccfbc6df179fd025fc7bfceca51af7bef0a62
fridtjof <2780577+fridtjof@users.noreply.github.com>
2020-07-30 02:48:12 +0200

    os_ndless.c: optimize usleep

--- src/os/os_ndless.c
+++ src/os/os_ndless.c
@@ -12,6 +12,8 @@
 #include <stdio.h>
 
 #include <libndls.h>
+// needed for the IO macro
+#include <os.h>
 
 // Main entry point.
 int main(int argc, char** argv) {
@@ -19,8 +21,42 @@ int main(int argc, char** argv) {
 }
 
 // h a c k because ndless only has msleep
+// so, we ripped off msleep with a different load value.
+// didn't make much of a difference though.
 int usleep(useconds_t __useconds) {
-    return msleep(__useconds / 1000);
+    printf("usleep: %u %u\n", __useconds, __useconds / 1000);
+
+    // see http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.ddi0271d/CHDFDDCF.html
+    volatile unsigned *load = (unsigned*)0x900D0000;
+    volatile unsigned *control = (unsigned*)0x900D0008;
+    volatile unsigned *int_clear = (unsigned*)0x900D000C;
+    volatile unsigned *int_status = (unsigned*)0x900D0010;
+    unsigned orig_control = *control;
+    unsigned orig_load = *load;
+    *control = 0; // disable timer
+    *int_clear = 1; // clear interrupt status
+    *load = __useconds / 32;
+    *control = 0b01100011; // disabled, TimerMode N/A, int, no prescale, 32-bit, One Shot -> 32khz
+    *control = 0b11100011; // enable timer
+
+    // Can't use idle() here as it acks the timer interrupt
+    volatile unsigned *intmask = IO(0xDC000008, 0xDC000010);
+    unsigned orig_mask = intmask[0];
+    intmask[1] = ~(1 << 19); // Disable all IRQs except timer
+
+    while ((*int_status & 1) == 0)
+            __asm volatile("mcr p15, 0, %0, c7, c0, 4" : : "r"(0) ); // Wait for an interrupt to occur
+
+    intmask[1] = 0xFFFFFFFF; // Disable all IRQs
+    intmask[0] = orig_mask; // renable IRQs
+
+    *control = 0; // disable timer
+    *int_clear = 1; // clear interrupt status
+    *control = orig_control & 0b01111111; // timer still disabled
+    *load = orig_load;
+    *control = orig_control; // enable timer
+
+    return 0;
 }
 
 // -- event
