c1b680af3c3376d9f5bef5ded253098e4c4ab5e4
Dave Kliczbor <dave@fsinfo.cs.tu-dortmund.de>
2021-01-07 20:36:34 +0100

    mathey.h: Added plain 4D/homogenous 3D vector/matrix math

--- src/mathey.c
+++ src/mathey.c
@@ -52,10 +52,11 @@ vec2 vmul(vec2 vec, float val) {
 	return r;
 }
 
-vec2 vdiv(vec2 v1, vec2 v2) {
-	vec2 r = {
-		.x = v1.x / v2.x,
-		.y = v1.y / v2.y,
+vec3 v3add(vec3 v1, vec3 v2) {
+	vec3 r = {
+		.x = v1.x + v2.x,
+		.y = v1.y + v2.y,
+		.z = v1.z + v2.z,
 	};
 	return r;
 }
@@ -66,6 +67,42 @@ vec2 multm2v2(matrix2_2 m, vec2 v) {
 		.y = (m.v2_1 * v.x) + (m.v2_2 * v.y),
 	};
 	return r;
+}
+
+vec3 multm3v3(matrix3_3 m, vec3 v) {
+  vec3 r = {
+      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3*v.z),
+      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3*v.z),
+      .z = (m.v3_1*v.x) + (m.v3_2*v.y) + (m.v3_3*v.z)
+  };
+  return r;
+}
+
+vec2 multm3v2(matrix3_3 m, vec2 v) {
+  vec2 r = {
+      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3),
+      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3)
+  };
+  return r;
+}
+
+vec4 multm4v4(matrix4_4 m, vec4 v) {
+  vec4 r = {
+      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3*v.z) + (m.v1_4*v.w),
+      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3*v.z) + (m.v2_4*v.w),
+      .z = (m.v3_1*v.x) + (m.v3_2*v.y) + (m.v3_3*v.z) + (m.v3_4*v.w),
+      .w = (m.v4_1*v.x) + (m.v4_2*v.y) + (m.v4_3*v.z) + (m.v4_4*v.w)
+  };
+  return r;
+}
+
+vec3 multm4v3(matrix4_4 m, vec3 v) {
+  vec3 r = {
+      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3*v.z) + (m.v1_4),
+      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3*v.z) + (m.v2_4),
+      .z = (m.v3_1*v.x) + (m.v3_2*v.y) + (m.v3_3*v.z) + (m.v3_4)
+  };
+  return r;
 }
 
 matrix2_2 multm2(matrix2_2 m1, matrix2_2 m2) {
@@ -136,6 +173,28 @@ matrix3_3 identity3() {
   return r;
 }
 
+matrix4_4 identity4() {
+  matrix4_4 r = {
+    .v1_1 = 1,
+    .v1_2 = 0,
+    .v1_3 = 0,
+    .v1_4 = 0,
+    .v2_1 = 0,
+    .v2_2 = 1,
+    .v2_3 = 0,
+    .v2_4 = 0,
+    .v3_1 = 0,
+    .v3_2 = 0,
+    .v3_3 = 1,
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1,
+  };
+  return r;
+}
+
 matrix3_3 composem3(int n, ...) {
   va_list valist;
   va_start(valist, n);
@@ -144,6 +203,20 @@ matrix3_3 composem3(int n, ...) {
   for (int i = 0; i < n; i++) {
     matrix3_3 x = va_arg(valist, matrix3_3);
     r = multm3(r,x);
+  }
+
+  va_end(valist);
+  return r;
+}
+
+matrix4_4 composem4(int n, ...) {
+  va_list valist;
+  va_start(valist, n);
+
+  matrix4_4 r = identity4();
+  for (int i = 0; i < n; i++) {
+    matrix4_4 x = va_arg(valist, matrix4_4);
+    r = multm4(r,x);
   }
 
   va_end(valist);
@@ -165,6 +238,80 @@ matrix3_3 rotation3(float angle) {
   return r;
 }
 
+matrix4_4 rotation4x(float angle) {
+  matrix4_4 r = {
+    .v1_1 = 1,
+    .v1_2 = 0,
+    .v1_3 = 0,
+    .v1_4 = 0,
+    .v2_1 = 0,
+    .v2_2 = cos(angle),
+    .v2_3 = -sin(angle),
+    .v2_4 = 0,
+    .v3_1 = 0,
+    .v3_2 = sin(angle),
+    .v3_3 = cos(angle),
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
+  };
+  return r;
+}
+
+matrix4_4 rotation4y(float angle) {
+  matrix4_4 r = {
+    .v1_1 = cos(angle),
+    .v1_2 = 0,
+    .v1_3 = sin(angle),
+    .v1_4 = 0,
+    .v2_1 = 0,
+    .v2_2 = 1,
+    .v2_3 = 0,
+    .v2_4 = 0,
+    .v3_1 = -sin(angle),
+    .v3_2 = 0,
+    .v3_3 = cos(angle),
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
+  };
+  return r;
+}
+
+matrix4_4 rotation4z(float angle) {
+  matrix4_4 r = {
+    .v1_1 = cos(angle),
+    .v1_2 = -sin(angle),
+    .v1_3 = 0,
+    .v1_4 = 0,
+    .v2_1 = sin(angle),
+    .v2_2 = cos(angle),
+    .v2_3 = 0,
+    .v2_4 = 0,
+    .v3_1 = 0,
+    .v3_2 = 0,
+    .v3_3 = 1,
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
+  };
+  return r;
+}
+
+matrix4_4 rotation4xyz(float x_angle, float y_angle, float z_angle) {
+  return composem4(3, rotation4x(x_angle), rotation4y(y_angle), rotation4z(z_angle));
+}
+
+matrix4_4 rotation4zyx(float x_angle, float y_angle, float z_angle) {
+  return composem4(3, rotation4z(z_angle), rotation4y(y_angle), rotation4x(x_angle));
+}
+
 matrix3_3 translation3(float x, float y) {
   matrix3_3 r = {
     .v1_1 = 1,
@@ -180,12 +327,42 @@ matrix3_3 translation3(float x, float y) {
   return r;
 }
 
+matrix4_4 translation4(float x, float y, float z) {
+  matrix4_4 r = {
+    .v1_1 = 1,
+    .v1_2 = 0,
+    .v1_3 = 0,
+    .v1_4 = x,
+    .v2_1 = 0,
+    .v2_2 = 1,
+    .v2_3 = 0,
+    .v2_4 = y,
+    .v3_1 = 0,
+    .v3_2 = 0,
+    .v3_3 = 1,
+    .v3_4 = z,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
+  };
+  return r;
+}
+
 matrix3_3 translation3_v2(vec2 v) {
   return translation3(v.x, v.y);
 }
 
 matrix3_3 translation3_v3(vec3 v) {
   return translation3(v.x, v.y);
+}
+
+matrix4_4 translation4_v3(vec3 v) {
+  return translation4(v.x, v.y, v.z);
+}
+
+matrix4_4 translation4_v4(vec4 v) {
+  return translation4(v.x, v.y, v.z);
 }
 
 matrix3_3 scale3(float x_factor, float y_factor) {
@@ -199,6 +376,28 @@ matrix3_3 scale3(float x_factor, float y_factor) {
     .v3_1 = 0,
     .v3_2 = 0,
     .v3_3 = 1
+  };
+  return r;
+}
+
+matrix4_4 scale4(float x_factor, float y_factor, float z_factor) {
+  matrix4_4 r = {
+    .v1_1 = x_factor,
+    .v1_2 = 0,
+    .v1_3 = 0,
+    .v1_4 = 0,
+    .v2_1 = 0,
+    .v2_2 = y_factor,
+    .v2_3 = 0,
+    .v2_4 = 0,
+    .v3_1 = 0,
+    .v3_2 = 0,
+    .v3_3 = z_factor,
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
   };
   return r;
 }
@@ -218,6 +417,47 @@ matrix3_3 shear3(float x_shear, float y_shear) {
   return r;
 }
 
+matrix4_4 shear4(float xy_shear, float xz_shear, float yx_shear, float yz_shear, float zx_shear, float zy_shear) {
+  matrix4_4 r = {
+    .v1_1 = 1,
+    .v1_2 = xy_shear,
+    .v1_3 = xz_shear,
+    .v1_4 = 0,
+    .v2_1 = yx_shear,
+    .v2_2 = 1,
+    .v2_3 = yz_shear,
+    .v2_4 = 0,
+    .v3_1 = zx_shear,
+    .v3_2 = zy_shear,
+    .v3_3 = 1,
+    .v3_4 = 0,
+    .v4_1 = 0,
+    .v4_2 = 0,
+    .v4_3 = 0,
+    .v4_4 = 1
+  };
+  return r;
+}
+
+vec4 vec3tovec4(vec3 v) {
+  vec4 a = {
+    .x = v.x,
+    .y = v.y,
+    .z = v.z,
+    .w = 1
+  };
+  return a;
+}
+
+vec3 vec4tovec3(vec4 a) {
+  vec3 v = {
+    .x = a.x,
+    .y = a.y,
+    .z = a.z
+  };
+  return v;
+}
+
 vec3 vec2tovec3(vec2 v) {
   vec3 a = {
     .x = v.x,
@@ -233,22 +473,5 @@ vec2 vec3tovec2(vec3 a) {
     .y = a.y
   };
   return v;
-}
-
-vec3 multm3v3(matrix3_3 m, vec3 v) {
-  vec3 r = {
-      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3*v.z),
-      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3*v.z),
-      .z = (m.v3_1*v.x) + (m.v3_2*v.y) + (m.v3_3*v.z)
-  };
-  return r;
-}
-
-vec2 multm3v2(matrix3_3 m, vec2 v) {
-  vec2 r = {
-      .x = (m.v1_1*v.x) + (m.v1_2*v.y) + (m.v1_3),
-      .y = (m.v2_1*v.x) + (m.v2_2*v.y) + (m.v2_3)
-  };
-  return r;
 }
 
--- src/mathey.h
+++ src/mathey.h
@@ -14,11 +14,17 @@ extern byte bmax(byte a, byte b);
 
 float square(float x);
 
+/** 
+ * Plain 2D vector.
+ */
 typedef struct vec2 {
 	float x;
 	float y;
 } vec2;
 
+/**
+ * Plain 2D matrix
+ */
 typedef struct matrix2_2 {
 	float v1_1;
 	float v1_2;
@@ -26,12 +32,18 @@ typedef struct matrix2_2 {
 	float v2_2;
 } matrix2_2;
 
+/**
+ * Plain 3D vector _OR_ homogenous 2D vector (if .z == 1)
+ */
 typedef struct vec3 {
   float x;
   float y;
   float z;
 } vec3;
 
+/**
+ * Plain 3D matrix _OR_ homogenous 2D transformation matrix (if last row == {0, 0, 1})
+  */
 typedef struct matrix3_3 {
   float v1_1;
   float v1_2;
@@ -44,6 +56,9 @@ typedef struct matrix3_3 {
   float v3_3;
 } matrix3_3;
 
+/**
+ * Homogenous 3D vector (with .w == 1)
+ */
 typedef struct vec4 {
   float x;
   float y;
@@ -51,6 +66,9 @@ typedef struct vec4 {
   float w;
 } vec4;
 
+/**
+ * Homogenous 3D transformation matrix (with last row == {0,0,0,1})
+ */
 typedef struct matrix4_4 {
   float v1_1;
   float v1_2;
@@ -70,34 +88,165 @@ typedef struct matrix4_4 {
   float v4_4;
 } matrix4_4;
 
+/**
+ * Vector initialization macros.
+ */
 #define vec2(xv, yv) ((vec2) { .x = (xv), .y = (yv)})
 #define vec3(xv, yv, zv) ((vec3) { .x = (xv), .y = (yv), .z = (zv)})
 #define vec4(xv, yv, zv, wv) ((vec4) { .x = (xv), .y = (yv), .z = (zv), .w = (wv)})
 
+/**
+ * Add two plain 2D vectors
+ */
 vec2 vadd(vec2 v1, vec2 v2);
+
+/**
+ * Multiply a plain 2D vector with a value ("scale")
+ */
 vec2 vmul(vec2 v1, float val);
-vec2 vdiv(vec2 v1, vec2 v2);
+
+/**
+ * Add two plain 3D vectors (don't add homogenous 2D vectors with this, use a translation matrix instead!)
+ */
+vec3 v3add(vec3 v1, vec3 v2);
+
+/**
+ * Multiply a matrix with a vector (component-based scaling). 
+ */
 vec2 multm2v2(matrix2_2 m, vec2 v);
+vec3 multm3v3(matrix3_3 m, vec3 v);
+vec2 multm3v2(matrix3_3 m, vec2 v);
+vec4 multm4v4(matrix4_4 m, vec4 v);
+vec3 multm4v3(matrix4_4 m, vec3 v);
 
+/**
+ * Multiply two matrices of same size ("compose").
+ */
 matrix2_2 multm2(matrix2_2 m1, matrix2_2 m2);
-
 matrix3_3 multm3(matrix3_3 m1, matrix3_3 m2);
+matrix4_4 multm4(matrix4_4 m1, matrix4_4 m2);
+
+/**
+ * Returns a 3D identity matrix
+ */
 matrix3_3 identity3();
+
+/**
+ * Composes (multiplies) n 3D/homogenous 2D matrices
+ */
 matrix3_3 composem3(int n, ...);
+
+
+/**
+ * Returns a homogenous 2D rotation matrix
+ */
 matrix3_3 rotation3(float angle);
+
+/**
+ * Returns a homogenous 2D translation matrix
+ */
 matrix3_3 translation3(float x, float y);
 matrix3_3 translation3_v2(vec2 v);
 matrix3_3 translation3_v3(vec3 v);
+
+/**
+ * Returns a homogenous 2D scale matrix
+ */
 matrix3_3 scale3(float x_factor, float y_factor);
+
+/**
+ * Returns a homogenous 2D shear matrix
+ */
 matrix3_3 shear3(float x_shear, float y_shear);
 
-matrix4_4 multm4(matrix4_4 m1, matrix4_4 m2);
 
+/**
+ * Returns a homogenous 3D identity matrix
+ */
+matrix4_4 identity4();
+
+/**
+ * Composes (multiplies) n homogenous 3D identity matrices
+ */
+matrix4_4 composem4(int n, ...);
+
+/**
+ * Returns a homogenous 3D rotation matrix around the x-axis
+ */
+matrix4_4 rotation4x(float angle);
+
+/**
+ * Returns a homogenous 3D rotation matrix around the y-axis
+ */
+matrix4_4 rotation4y(float angle);
+
+/**
+ * Returns a homogenous 3D rotation matrix around the z-axis
+ */
+matrix4_4 rotation4z(float angle);
+
+/**
+ * Returns a homogenous 3D rotation matrix representing a rotation around the x-axis, then y-axis, then z-axis
+ */
+matrix4_4 rotation4xyz(float x_angle, float y_angle, float z_angle);
+
+/**
+ * Returns a homogenous 3D rotation matrix representing a rotation around the z-axis, then y-axis, then x-axis
+ */
+matrix4_4 rotation4zyx(float x_angle, float y_angle, float z_angle);
+
+/**
+ * Returns a homogenous 3D translation matrix
+ */
+matrix4_4 translation4(float x, float y, float z);
+matrix4_4 translation4_v3(vec3 v);
+matrix4_4 translation4_v4(vec4 v);
+
+/**
+ * Returns a homogenous 3D scaling matrix
+ */
+matrix4_4 scale4(float x_factor, float y_factor, float z_factor);
+
+/**
+ * Returns a homogenous 3D shearing matrix
+ */
+matrix4_4 shear4(float xy_shear, float xz_shear, float yx_shear, float yz_shear, float zx_shear, float zy_shear);
+
+
+/**
+ * Converts between plain vectors and homogenous vectors
+ */
+vec4 vec3tovec4(vec3 v);
+vec3 vec4tovec3(vec4 a);
 vec3 vec2tovec3(vec2 v);
 vec2 vec3tovec2(vec3 a);
-vec3 multm3v3(matrix3_3 m, vec3 v);
-vec2 multm3v2(matrix3_3 m, vec2 v);
 
+/**
+ * This is a split-up version of multm3v2: if used correctly, this saves cycles.
+ *
+ * multm3v2_partx calculates only the x-axis-dependent parts of a multiplication of a matrix with a vector (the "x kernel").
+ * multm3v2_partxy calculates only the y-axis-dependent parts, using the precalculated "x kernel".
+ *
+ * This is usable in the following situation, where the x-axis-dependent part is calculated again and again
+ * for each point on the y axis:
+ *
+ * for(int x = 0; x < MATRIX_X; x++) {
+ *   for(int y = 0; y < MATRIX_Y; y++) {
+ *     vec2 point = multm3v2(my_composed_transformation_matrix, vec2(x,y));
+ *   }
+ * }
+ *
+ * The point here is: All operations on x are repeated for each y, even though x does not change inside the y loop.
+ * To remedy this, the multm3v2 operation can be split up, leading to the following code:
+ *
+ * for(int x = 0; x < MATRIX_X; x++) {
+ *   vec2 x_kernel = multm3v2_partx(my_composed_transformation_matrix, x);
+ *   for(int y = 0; y < MATRIX_Y; y++) {
+ *     vec2 point = multm3v2_partxy(my_composed_transformation_matrix, x_kernel, y);
+ *   }
+ * }
+ *
+ */
 static inline vec2 multm3v2_partx(matrix3_3 m, float v_x) {
   vec2 r = {
       .x = (m.v1_1*v_x) + (m.v1_3),
