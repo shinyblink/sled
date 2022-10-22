57e08b62f14484ccfea9237f4e5879aaf5f7ad4c
Olaf Pichler <olaf.pichler@urz.uni-heidelberg.de>
2022-10-23 00:01:53 +0200

    removed tabs

--- src/modules/gfx_pickover.c
+++ src/modules/gfx_pickover.c
@@ -69,45 +69,45 @@ static const float xy_scale = 10;
 
 int init(int moduleno, char* argstr) {
 
-	modno = moduleno;
-	frame = 0;
+    modno = moduleno;
+    frame = 0;
 
-	// get canvas size
-	xmax = matrix_getx();
-	ymax = matrix_gety();
-	inv_xmax = 1.0 / xmax;
-	inv_ymax = 1.0 / ymax;
+    // get canvas size
+    xmax = matrix_getx();
+    ymax = matrix_gety();
+    inv_xmax = 1.0 / xmax;
+    inv_ymax = 1.0 / ymax;
 
-	// randomize starting scroll parameters z and inc_z
-	z = (1 - 2.0*rand()/(float)(RAND_MAX)) * Z_INIT;
-	inc_z = z > 0 ? -0.01 : 0.01;
+    // randomize starting scroll parameters z and inc_z
+    z = (1 - 2.0*rand()/(float)(RAND_MAX)) * Z_INIT;
+    inc_z = z > 0 ? -0.01 : 0.01;
 
-	// initialize params
-	params[0] = 1;
-	params[1] = 1.8;
-	params[2] = 0.71;
-	params[3] = 1.51;
+    // initialize params
+    params[0] = 1;
+    params[1] = 1.8;
+    params[2] = 0.71;
+    params[3] = 1.51;
 
-	// randomize params
-	u_int8_t to_change = rand()%5;
-	u_int8_t changed[4] = {0,0,0,0};
-	u_int8_t c_count = 0;
+    // randomize params
+    u_int8_t to_change = rand()%5;
+    u_int8_t changed[4] = {0,0,0,0};
+    u_int8_t c_count = 0;
 
-	while(c_count < to_change) {
-		u_int8_t c = rand()%4;
-		if(changed[c] == 0) {
-			c_count++;
-			changed[c] = 1;
-			params[c] += (1. - 2. * rand() / (float)(RAND_MAX)) * 0.5 / c_count;
-		}
-	}
+    while(c_count < to_change) {
+        u_int8_t c = rand()%4;
+        if(changed[c] == 0) {
+            c_count++;
+            changed[c] = 1;
+            params[c] += (1. - 2. * rand() / (float)(RAND_MAX)) * 0.5 / c_count;
+        }
+    }
 
-	return 0;
+    return 0;
 }
 
 void reset(int _modno) {
-	nexttick = udate();
-	frame = 0;
+    nexttick = udate();
+    frame = 0;
 
     // slightly adjust scroll speed
     inc_z += (1-2.0*rand()/(float)(RAND_MAX)) * 0.002;
@@ -124,28 +124,28 @@ void reset(int _modno) {
 
 // numerical function iterator
 static float pickover_int( Point* p ) {
-	float dx = sin( params[0] * p->y ) - p->z * cos( params[1] * p->x );
-	float dy = p->z * sin( params[2] * p->x ) - cos( params[3] * p->y );
-	float dz = sin( p->x );
+    float dx = sin( params[0] * p->y ) - p->z * cos( params[1] * p->x );
+    float dy = p->z * sin( params[2] * p->x ) - cos( params[3] * p->y );
+    float dz = sin( p->x );
 
-	float ret = (p->x - dx) * (p->x - dx) +
-		        (p->y - dy) * (p->y - dy) +
-		        (p->z - dz) * (p->z - dz);
+    float ret = (p->x - dx) * (p->x - dx) +
+                (p->y - dy) * (p->y - dy) +
+                (p->z - dz) * (p->z - dz);
 
-	p->x = dx;
-	p->y = dy;
-	p->z = dz;
+    p->x = dx;
+    p->y = dy;
+    p->z = dz;
 
-	return ret;
+    return ret;
 }
 
 int draw(int _modno, int argc, char* argv[]) {
 
-	// do shader stuff badly on a single core
-	for (uint x = 0; x < xmax; x++ ) {
-		for ( uint y = 0; y < ymax; y++ ) {
+    // do shader stuff badly on a single core
+    for (uint x = 0; x < xmax; x++ ) {
+        for ( uint y = 0; y < ymax; y++ ) {
 
-			point.x = x * inv_xmax * xy_scale;
+            point.x = x * inv_xmax * xy_scale;
             point.y = y * inv_ymax * xy_scale;
             point.z = z;
 
@@ -164,24 +164,24 @@ int draw(int _modno, int argc, char* argv[]) {
 
             r = r > 1 ? 255.0 * r / dist : 255.0;
             g = g > 1 ? 255.0 * g / dist : 255.0;
-		    b = 255.0 * b/(b+1.0);
+            b = 255.0 * b/(b+1.0);
 
-			RGB c = RGB(255 - (uint)r, 255 - (uint)g, (uint)b);
-			matrix_set(x,y,c);
-		}
-	}
+            RGB c = RGB(255 - (uint)r, 255 - (uint)g, (uint)b);
+            matrix_set(x,y,c);
+        }
+    }
 
-	z += inc_z;
+    z += inc_z;
 
-	matrix_render();
-	if (frame >= FRAMES) {
-		frame = 0;
-		return 1;
-	}
-	frame++;
-	nexttick += FRAMETIME;
-	timer_add(nexttick, modno, 0, NULL);
-	return 0;
+    matrix_render();
+    if (frame >= FRAMES) {
+        frame = 0;
+        return 1;
+    }
+    frame++;
+    nexttick += FRAMETIME;
+    timer_add(nexttick, modno, 0, NULL);
+    return 0;
 }
 
 void deinit(int _modno) {}
