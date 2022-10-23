058ea8603fa5fde47025f147e4f021b297a05564
Olaf Pichler <brolf@magheute.net>
2022-10-23 17:03:16 +0200

    Add gfx_pickover; Fix small bug in gfx_lorenz (#147)
    * gfx_lorenz: fixed bug drawing otside of matrix
    
    * added pickover effect
    
    * removed doubled import
    
    * removed tabs
    
    * fixed an out of bounds read
    
    * added some output and made z-bounderies defienes
    
    * removed tabs
    
    * removed tabs
    
    * moved zoom factor to define
    
    * removed tabs and trailing whitespaces
    
    * added huge performance improvement

--- src/modules/gfx_lorenz.c
+++ src/modules/gfx_lorenz.c
@@ -1,11 +1,11 @@
 // Lorenz System
 //
 // Copyright (c) 2022, Olaf "Brolf" Pichler <brolf@magheute.net>
-// 
+//
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
 // copyright notice and this permission notice appear in all copies.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
 // WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
 // MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
@@ -17,7 +17,6 @@
 #include <types.h>
 #include <matrix.h>
 #include <timers.h>
-#include <random.h>
 #include <stddef.h>
 #include <random.h>
 #include <string.h>
@@ -55,87 +54,94 @@ static const unsigned int frame_xmax = 32;
 static const unsigned int frame_ymax = 32;
 
 int init(int moduleno, char* argstr) {
-	if (matrix_getx() < 64)	return 1;
-	if (matrix_gety() < 64)	return 1;
+    if (matrix_getx() < 64) return 1;
+    if (matrix_gety() < 64) return 1;
 
-	xmax = matrix_getx();
-	ymax = matrix_gety();
+    xmax = matrix_getx();
+    ymax = matrix_gety();
 
-	modno = moduleno;
-	frame = 0;
+    modno = moduleno;
+    frame = 0;
 
-	delta0 = 2 * M_PI * rand()/(float)(RAND_MAX);
+    delta0 = 2 * M_PI * rand()/(float)(RAND_MAX);
 
-	for (int i = 0; i<P_MAX; i++) {
-		p[i].x = rand()/(float)(RAND_MAX)*10.;
-		p[i].y = rand()/(float)(RAND_MAX)*10.;
-		p[i].z = rand()/(float)(RAND_MAX)*10.;
-	}
+    for (int i = 0; i<P_MAX; i++) {
+        p[i].x = rand()/(float)(RAND_MAX)*10.;
+        p[i].y = rand()/(float)(RAND_MAX)*10.;
+        p[i].z = rand()/(float)(RAND_MAX)*10.;
+    }
 
-	return 0;
+    return 0;
 }
 
 void reset(int _modno) {
-	rotate_rpm = randn(3)*3;
-	delta0 = 2 * M_PI * rand()/(float)(RAND_MAX);
-	printf("\nrpm: %i   delta0: %f\n",rotate_rpm, delta0);
-	nexttick = udate();
-	frame = 0;
+    rotate_rpm = randn(3)*3;
+    delta0 = 2 * M_PI * rand()/(float)(RAND_MAX);
+    printf("\nrpm: %i   delta0: %f\n",rotate_rpm, delta0);
+    nexttick = udate();
+    frame = 0;
 }
 
 static void lorenz_int( Point* p, float delta_t) {
-	float dx = sigma * (p->y - p->x);
-	float dy = p->x * (rho - p->z) - p->y;
-	float dz = p->x* p->y - beta * p->z;
+    float dx = sigma * (p->y - p->x);
+    float dy = p->x * (rho - p->z) - p->y;
+    float dz = p->x* p->y - beta * p->z;
 
-	p->x = p->x + delta_t * dx;
-	p->y = p->y + delta_t * dy;
-	p->z = p->z + delta_t * dz;
+    p->x = p->x + delta_t * dx;
+    p->y = p->y + delta_t * dy;
+    p->z = p->z + delta_t * dz;
 }
 
 int draw(int _modno, int argc, char* argv[]) {
-	for (int x = 0; x < matrix_getx(); x++ ) {
-		for ( int y = 0; y < matrix_gety(); y++ ) {
-			RGB c = matrix_get(x,y);
-			c.red *= 0.99;
-			c.green *= 0.9;
-			c.blue *= 0.8;
-			matrix_set(x,y,c);
-		}
-	}
+    for (int x = 0; x < matrix_getx(); x++ ) {
+        for ( int y = 0; y < matrix_gety(); y++ ) {
+            RGB c = matrix_get(x,y);
+            c.red *= 0.99;
+            c.green *= 0.9;
+            c.blue *= 0.8;
+            matrix_set(x,y,c);
+        }
+    }
 
+    for (int l = 0; l < 10; l++) {
+        for (int i = 0; i < P_MAX; i++) {
+            lorenz_int(&p[i], DELTA_T);
 
-	for (int l = 0; l < 10; l++) {
-		for (int i = 0; i < P_MAX; i++) {
-			lorenz_int(&p[i], DELTA_T);
+            float scale;
+            if (rotate_rpm == 0){
+                scale = 0.0;
+            }
+            else {
+                scale = ((frame % (FPS * 60 / rotate_rpm )) /
+                         (FPS * 60.0 / rotate_rpm));
+            }
 
-			float scale = rotate_rpm == 0 ? 0.0 : ((frame % (FPS * 60 / rotate_rpm )) / (FPS * 60.0 / rotate_rpm));
+            float delta = delta0 + 2 * M_PI * scale;
+            while (delta < -M_PI) delta += 2 * M_PI;
+            while (delta > M_PI) delta -= 2 * M_PI;
 
-			float delta = delta0 + 2 * M_PI * scale;
-			while (delta < -M_PI) delta += 2 * M_PI;
-			while (delta > M_PI) delta -= 2 * M_PI;
+            float x = p[i].x * cos(delta) - p[i].y * sin(delta);
+            float y = p[i].x * sin(delta) + p[i].y * cos(delta);
+            x = x * (xmax/frame_xmax/2) + xmax/2;
+            y = y * (ymax/frame_ymax/2) + ymax/2;
 
-			float x = p[i].x * cos(delta) - p[i].y * sin(delta);
-			float y = p[i].x * sin(delta) + p[i].y * cos(delta);
-			x = x * (xmax/frame_xmax/2) + xmax/2;
-			y = y * (ymax/frame_ymax/2) + ymax/2;
+            if (x < 0) continue;
+            if (y < 0) continue;
+            if (x >= xmax) continue;
+            if (y >= ymax) continue;
+            matrix_set(x, y, white);
+        }
+    }
 
-			if (x < 0) continue;
-			if (y < 0) continue;
-			if (x >= xmax) continue;
-			if (y >= ymax) continue;
-			matrix_set(x, y, white);
-		}
-	}
-	matrix_render();
-	if (frame >= FRAMES) {
-		frame = 0;
-		return 1;
-	}
-	frame++;
-	nexttick += FRAMETIME;
-	timer_add(nexttick, modno, 0, NULL);
-	return 0;
+    matrix_render();
+    if (frame >= FRAMES) {
+        frame = 0;
+        return 1;
+    }
+    frame++;
+    nexttick += FRAMETIME;
+    timer_add(nexttick, modno, 0, NULL);
+    return 0;
 }
 
 void deinit(int _modno) {}
---
+++ src/modules/gfx_pickover.c
@@ -0,0 +1,189 @@
+// Pickover System
+//
+// Copyright (c) 2022, Olaf "Brolf" Pichler <brolf@magheute.net>
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+//
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+// WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+// ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+// WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+// ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+// OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+#include <types.h>
+#include <matrix.h>
+#include <timers.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <mathey.h>
+#include <stdio.h>
+#include <random.h>
+
+#define FPS 60
+#define FRAMETIME (T_SECOND / FPS)
+#define FRAMES (TIME_MEDIUM * FPS)
+
+// performance parameter
+#define STABLE_DT 1.0
+#define ITER_MAX 1000
+#define SKIP_COUNT 2  // skips xmax*ymax/N pixel per frame
+
+// scoll max value
+#define Z_MAX 5
+
+// initial scroll window [-Z_INIT, Z_INIT]
+#define Z_INIT 3
+
+// zoom factor
+#define XY_SCALE 10
+
+// equation parameters
+static float params[4];
+
+// canvas size
+static unsigned int xmax;
+static unsigned int ymax;
+
+// inverse of canvas size
+static float inv_xmax;
+static float inv_ymax;
+
+// sliding param and increment
+static float z;
+static float inc_z;
+
+// test point
+typedef vec3 Point;
+static Point point;
+
+static int modno;
+static unsigned int frame;
+static oscore_time nexttick;
+
+int init(int moduleno, char* argstr) {
+
+    modno = moduleno;
+    frame = 0;
+
+    // get canvas size
+    xmax = matrix_getx();
+    ymax = matrix_gety();
+    inv_xmax = 1.0 / xmax;
+    inv_ymax = 1.0 / ymax;
+
+    // randomize starting scroll parameters z and inc_z
+    z = (1 - 2.0*rand()/(float)(RAND_MAX)) * Z_INIT;
+    inc_z = z > 0 ? -0.01 : 0.01;
+
+    // initialize params
+    params[0] = 1;
+    params[1] = 1.8;
+    params[2] = 0.71;
+    params[3] = 1.51;
+
+    // randomize params
+    u_int8_t to_change = rand()%5;
+    u_int8_t changed[4] = {0,0,0,0};
+    u_int8_t c_count = 0;
+
+    while(c_count < to_change) {
+        u_int8_t c = rand()%4;
+        if(changed[c] == 0) {
+            c_count++;
+            changed[c] = 1;
+            params[c] += (1. - 2. * rand() / (float)(RAND_MAX)) * 0.5 / c_count;
+        }
+    }
+
+    return 0;
+}
+
+void reset(int _modno) {
+    nexttick = udate();
+    frame = 0;
+
+    // slightly adjust scroll speed
+    inc_z += (1-2.0*rand()/(float)(RAND_MAX)) * 0.002;
+
+    // Prevent running to far from z=0
+    if( (z < 0 && z < -Z_MAX && inc_z < 0) ||
+        (z > 0 && z > Z_MAX && inc_z > 0) ) {
+        inc_z = -inc_z;
+    }
+
+    // display z and inc_z
+    printf("z: %f   inc_z: %f\n", z, inc_z);
+}
+
+// numerical function iterator
+static float pickover_int( Point* p ) {
+    float dx = sin( params[0] * p->y ) - p->z * cos( params[1] * p->x );
+    float dy = p->z * sin( params[2] * p->x ) - cos( params[3] * p->y );
+    float dz = sin( p->x );
+
+    float ret = (p->x - dx) * (p->x - dx) +
+                (p->y - dy) * (p->y - dy) +
+                (p->z - dz) * (p->z - dz);
+
+    p->x = dx;
+    p->y = dy;
+    p->z = dz;
+
+    return ret;
+}
+
+int draw(int _modno, int argc, char* argv[]) {
+
+    // do shader stuff badly on a single core
+    for (uint x = 0; x < xmax; x++ ) {
+        for ( uint y = 0; y < ymax; y++ ) {
+            if((y * xmax + x + frame) % SKIP_COUNT) {
+                continue;
+            }
+
+            point.x = x * inv_xmax * XY_SCALE;
+            point.y = y * inv_ymax * XY_SCALE;
+            point.z = z;
+
+            for( int count=0; count < ITER_MAX; ++count ) {
+                float dt = pickover_int(&point);
+                if (dt < STABLE_DT) {
+                    break;
+                }
+            }
+
+            float r = point.x < 0 ? -point.x : point.x;
+            float g = point.y < 0 ? -point.y : point.y;
+            float b = point.z < 0 ? -point.z : point.z;
+
+            float dist = sqrt(r*r + g*g + b*b);
+
+            r = r > 1 ? 255.0 * r / dist : 255.0;
+            g = g > 1 ? 255.0 * g / dist : 255.0;
+            b = 255.0 * b/(b+1.0);
+
+            RGB c = RGB(255 - (uint)r, 255 - (uint)g, (uint)b);
+            matrix_set(x,y,c);
+        }
+    }
+
+    z += inc_z;
+
+    matrix_render();
+    if (frame >= FRAMES) {
+        frame = 0;
+        return 1;
+    }
+    frame++;
+    nexttick += FRAMETIME;
+    timer_add(nexttick, modno, 0, NULL);
+    return 0;
+}
+
+void deinit(int _modno) {}
